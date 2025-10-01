d300c92f22988064720a7ed694fdeb720a65a855
Pascal Martin <connect@pascalmartin.dev>
2025-10-02 01:23:14 +1000

    DynamoDBClientSingleton class created

---
+++ src/services/db/dynamoDBClientSingleton.ts
@@ -0,0 +1,24 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { fromCognitoIdentityPool } from '@aws-sdk/credential-providers';
+
+class DynamoDBClientSingleton {
+  private static instance: DynamoDBClient;
+
+  private constructor() {}
+
+  public static getInstance(): DynamoDBClient {
+    if (!DynamoDBClientSingleton.instance) {
+      DynamoDBClientSingleton.instance = new DynamoDBClient({
+        region: import.meta.env.VITE_AWS_REGION,
+        credentials: fromCognitoIdentityPool({
+          identityPoolId: import.meta.env.VITE_COGNITO_IDENTITY_POOL_ID,
+        }),
+      });
+    }
+    return DynamoDBClientSingleton.instance;
+  }
+}
+
+export default DynamoDBClientSingleton;
+
+
