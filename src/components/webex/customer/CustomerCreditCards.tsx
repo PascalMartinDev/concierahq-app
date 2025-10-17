59f35ab4a6d70e54d7b70ba13846b7c4ae0ff56b
Pascal Martin <connect@pascalmartin.dev>
2025-10-17 10:55:37 +1000

    Updated CustomerCreditCard component removed un needed headings

--- src/components/webex/customer/CustomerCreditCards.tsx
+++ src/components/webex/customer/CustomerCreditCards.tsx
@@ -8,25 +8,10 @@ const CustomerCreditCards: React.FC = () => {
 
   // Get credit cards from the eCommerce creditCardList
   const creditCards = appCustomer?.eCommerce.creditCardList.CreditCardList || [];
-  /*
-  const creditCards = SamplCreditCards.map(card => ({
-    cardId: card.card_id,
-    cardBrand: card.card_brand,
-    maskedCardNumber: card.masked_Card_Number,
-    expiryMonth: Number(card.expiry_month),
-    expiryYear: Number(card.expiry_year),
-    isDefault: ((typeof card.is_default === 'string' && card.is_default === 'true') || (typeof card.is_default === 'boolean' && card.is_default === true)) ? 'true' : 'false',
-  }));
-  */
+  
   return (
     <div className="p-6 bg-white rounded-lg border border-gray-300 shadow-sm">
       <div>
-        <p className="text-gray-600 mb-4">
-          <b>Payment Methods:</b>
-          <p>"TEST: CreditCard List:"</p>
-          <p>creditCards</p>
-        </p>
-
         {creditCards.length === 0 ? (
           <p className="text-gray-400 italic">No credit cards on file</p>
         ) : (
